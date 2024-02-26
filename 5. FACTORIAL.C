1571de81c75bb0f102da9c97739803753b7b373d
dhamu947 <124136816+dhamu947@users.noreply.github.com>
2024-02-26 12:34:36 +0530

    Create 5. FACTORIAL.C

---
+++ 5. FACTORIAL.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function to calculate the factorial of a number recursively
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int fact = factorial(num);
+        printf("Factorial of %d is %d\n", num, fact);
+    }
+
+    return 0;
+}
